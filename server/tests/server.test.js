e5b1891fb55333a8321831884a34ab59daf7cde0
tadillon1 <tadillon@gmail.com>
2017-09-05 13:42:39 -0700

    Test POST /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,31 +2,31 @@ const expect = require('expect');
 const request = require('supertest');
 
 
-const {app} = require('./../server');
-const {Todo} = require('./../models/todo');
+const {app} = require('./../server');   //create connection to web server
+const {Todo} = require('./../models/todo');  //create connection to Todo model in mongoose
 
-beforeEach((done) => {
+beforeEach((done) => {            //Run some code before each test case...  only moves onto the test case once "done"
   Todo.remove({}).then(() => done());  //Delete all DB objects before running each test case.
 });
 
 describe('POST /todos', () => {
-  it('should create a new todo', (done) => {
+  it('should create a new todo', (done) => {  //must pass "done" to the it function
     var text = 'Test todo text';
     request(app)
-    .post('/todos')  //test using the /todos url
-    .send({text})   //send the text variable from above...  This sends it in json format by default
+    .post('/todos')  //test using the /todos url to add todos
+    .send({text})   //send the text variable from above to the server...  This sends it in json format by default
     .expect(200)    // this test should send back a status of 200
     .expect((res) => {  //we expect the returned response body has a text property and the body is equal to the text from above.
-      expect(res.body.text).tobe(text);
+      expect(res.body.text).toBe(text);
     })
-    .end((err, res) => {
+    .end((err, res) => {    //err= errors exist, res = response from above
       if(err){
-        return done(err);
+        return done(err);   //return stops the execution
       }
 
-      Todo.find().then((todos) => {
-        expect(todos.length).toBe(1);
-        expect(todos[0].text).toBe(text);
+      Todo.find().then((todos) => { //fetch all of the todos from MonboDB
+        expect(todos.length).toBe(1);  //Expect the length to be 1
+        expect(todos[0].text).toBe(text);  //Expect the first document to be "text"
         done();
       }).catch((e) => done(e));
     });
@@ -37,11 +37,11 @@ describe('POST /todos', () => {
     .post('/todos')
     .send({})
     .expect(400)
-    .end((erro, res) => {
+    .end((err, res) => {
       if (err) {
         return done(err);
       }
-      Todo.find().find((todos) => {
+      Todo.find().then((todos) => {
         expect(todos.length).toBe(0);
         done();
       }).catch((e) => done(e));
